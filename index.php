2bbd8fae09257addb456ec709acbdf9927e8d249
Valtteri <Valtt3r1@users.noreply.github.com>
2015-03-25 22:58:40 +0200

    Update index.php
    Testi

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Ensimm‰inen PHP-sivu</title>
+    <title>Ensimm√§inen PHP-sivu</title>
   </head>
   <body>
     <p>Vuorokaudessa on <?php echo 24 * 60 * 60; ?> sekuntia.</p>
-    <p>T‰n‰‰n on <?php echo date("j.n.Y"); ?>.</p>
+    <p>T√§n√§√§n on <?php echo date("j.n.Y"); ?>.</p>
     <p>Palvelimella on PHP:n versio <?php echo PHP_VERSION; ?>.</p>
+    <p> make on vitun pelle <p>
     <?php
     echo "<ul>";
     for ($i = 1; $i <= 10; $i++) {
